832916bcc4cfdcc7f91a2f0254da5bf466f0ad91
Ubuntu <ubuntu@ip-172-31-9-93.us-east-2.compute.internal>
2024-03-19 19:11:22 +0000

    IPFS Installation Script

--- ipfs-update.sh
+++ ipfs-update.sh
@@ -1,11 +1,6 @@
-wget https://dist.ipfs.tech/ipfs-update/v1.9.0/ipfs-update_v1.9.0_linux-amd64.tar.gz
-
-tar -xvzf ipfs-update_v1.9.0_linux-amd64.tar.gz
-
-cd ipfs-update
-
-sudo bash install.sh
-
-sudo ipfs-update install latest
-
+sudo apt-get update
+sudo apt-get install golang-go -y
+wget https://dist.ipfs.io/go-ipfs/v0.27.0/go-ipfs_v0.27.0_linux-386.tar.gz
+tar xvfz go-ipfs_v0.27.0_linux-386.tar.gz
+sudo mv go-ipfs/ipfs /usr/local/bin/ipfs
 ipfs --version
