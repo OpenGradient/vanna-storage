c1aa624d9d42829ed8fa24b41cce2b3166e74878
kukac <adambalogh@users.noreply.github.com>
2024-04-26 08:26:08 -0400

    Dockerize (#5)
    * compose
    
    * init
    
    * port
    
    ---------
    
    Co-authored-by: Adam Balogh <adambalogh@Adams-MacBook-Air.local>

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 run:
 	python3 src/app.py
+
+docker:
+	docker build . -t vanna-storage
---
+++ ipfs/init.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -ex
+
+ipfs bootstrap add /ip4/3.140.191.156/tcp/4001/p2p/12D3KooWQWntZ1RYAxBtqbPcRz1e24o9xzXkvieJnhhNAC6xKwAF 
